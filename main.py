6365b3489c9c514cd09f6252467e0d19bbbe8114
kaisei <63438515+lemolatoon@users.noreply.github.com>
2021-08-12 13:00:41 +0900

    add discription of loss

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ class Agent:
         self.num_actions = num_actions
         self.optimizer = optimizer
         
-        self.FINAL_EPS = 0.1
-        self.INITIAL_REPLAY_SIZE = 1000
+        self.FINAL_EPS = 0.05
+        self.INITIAL_REPLAY_SIZE = 512
         self.NUM_REPLAY_MEMORY = 20000
-        self.TRAIN_INTERVAL = 4
+        self.TRAIN_INTERVAL = 2
         self.TARGET_UPDATE_INTERVAL = 250
-        self.BATCH_SIZE = 32
-        self.GAMMA = 0.99
-        self.epsilon_step = 0.5 * 1e-4
+        self.BATCH_SIZE = 16
+        self.GAMMA = 0.95
+        self.epsilon_step = 1e-4
 
         """
         self.q_net = self.construct_model(self.num_actions)
@@ -55,8 +55,7 @@ class Agent:
         
 
     def _get_action(self, state) -> int:
-        q = sys.float_info.min
-        n = np.argmax(self.q_net(state)) #Q値最大のactionを選択
+        n = np.argmax(self.q_net.predict(state)) #Q値最大のactionを選択
         assert n >= 0 and n <= self.num_actions
         return n
 
@@ -64,80 +63,65 @@ class Agent:
         if self.eps > self.FINAL_EPS and self.t >= self.INITIAL_REPLAY_SIZE:
             self.eps -= self.epsilon_step
 
-        if self.eps < np.random.rand() or self.t < self.INITIAL_REPLAY_SIZE:
-            return self._get_action(state)
-        else:
+        if self.eps > np.random.rand() or self.t < self.INITIAL_REPLAY_SIZE:
             return np.random.randint(self.num_actions)
+        else:
+            return self._get_action(state)
 
 
     def memory(self, state, action, reward, next_state, done):
 
         self.episode_memory.append((state, action, reward, next_state, done))
 
+
+        loss = None
+        losses = []
         if self.t > self.INITIAL_REPLAY_SIZE:
             if self.t % self.TRAIN_INTERVAL == 0:
-                self.train_network_batch()
+                for i in range(10):
+                    loss = self.train_network_batch()
+                losses.append(loss)
+            else:
+                loss = self.calc_loss()
+
 
             if self.t % self.TARGET_UPDATE_INTERVAL == 0:
                 self.update_target_net()
+
+        if len(losses) != 0:
+            loss = np.mean(np.array(losses))
         
         self.t += 1
 
-
-    def train_network(self):
-        state_batch = []
-        action_batch = []
-        reward_batch = []
-        next_state_batch = []
-        done_batch = []
-
-        y_batch = []
-
-        size = len(self.episode_memory)
-
-        #minibatch = random.sample(self.episode_memory, self.BATCH_SIZE)
-        minibatch = self.episode_memory
-        for data in minibatch:
-            state_batch.append(data[0])
-            action_batch.append(data[1])
-            reward_batch.append(data[2]) 
-            next_state_batch.append(data[3])
-            done_batch.append(data[4])
-
-        state_batch = np.array(state_batch)
-        action_batch = np.array(action_batch)
-        reward_batch = np.array(reward_batch)
-        next_state_batch = np.array(next_state_batch)
-        done_batch = np.array(done_batch, dtype=int)
-
-        #target_q_values_batch = np.zeros((self.BATCH_SIZE,)) #memory確保
-        target_q_values_batch = np.zeros((size,)) #memory確保
-
-        for i in range(self.num_actions): #すべてのactionについてq値を計算
-            #print(np.array(next_state_batch).shape)
-            #print(np.concatenate([next_state_batch, np.full(shape=(self.BATCH_SIZE, 1), fill_value=i)], axis=1))
-            x = np.concatenate([next_state_batch, np.full(shape=(size, 1), fill_value=i)], axis=1) #(s, a)を結合
-            y = self.target_net(np.array(x))
-            y = y.numpy().reshape(-1) #縦のべくとるを横のベクトルにする
-            target_q_values_batch = np.maximum(y, target_q_values_batch) #各アクションの中で最も大きいものを取る
-        y_batch = reward_batch + (1 - done_batch) * self.GAMMA * target_q_values_batch #equationの右辺
-
-        action_batch = action_batch.reshape(-1, 1) #state_batchと結合するためのreshape
-        x_train = np.concatenate([state_batch, action_batch], axis=1) #equation右辺のQの引数
-        
-
-        self.q_net.fit(        #epochsが10だと少ない
-            x_train, y_batch, epochs=5, verbose=0, batch_size=self.BATCH_SIZE) #verbose=0でno log
+        return loss
 
 
     def train_network_batch(self):
         states, actions, rewards, next_states, dones = self.get_minibatch()
 
         next_Qs = np.max(self.target_net.predict(next_states), axis=1) #max_a(Q_{target}(s', a))
-        target_value = rewards * (1 - dones) * self.GAMMA * next_Qs 
+        #target_value = rewards * (1 - dones) * self.GAMMA * next_Qs 
+        target_value = [reward + self.GAMMA * next_Q if not done else reward 
+            for reward, done, next_Q in zip(rewards, dones, next_Qs)]
 
 
-        self.q_net.update(states=states, selected_actions=actions, target_values=target_value)
+
+        loss = self.q_net.update(states=states, selected_actions=actions, target_values=target_value)
+        
+        return loss
+
+    def calc_loss(self):
+        states, actions, rewards, next_states, dones = self.get_minibatch()
+
+        next_Qs = np.max(self.target_net.predict(next_states), axis=1) #max_a(Q_{target}(s', a))
+        #target_value = rewards * (1 - dones) * self.GAMMA * next_Qs 
+        target_value = [reward + self.GAMMA * next_Q if not done else reward 
+            for reward, done, next_Q in zip(rewards, dones, next_Qs)]
+
+        loss = self.q_net.loss(states=states, selected_actions=actions, target_values=target_value)
+        
+        return loss
+
 
     def get_minibatch(self):
         state_batch = []
@@ -165,20 +149,13 @@ class Agent:
     def update_target_net(self):
         for i in range(len(self.target_net.weights)):
             self.target_net.weights[i].assign(self.q_net.weights[i]) #代入
-        print("==========================================")
-        print(f"q_net{[w.numpy().shape for w in self.q_net.weights]}")
-        print(f"target_net{[w.numpy().shape for w in self.target_net.weights]}")
-        print("==========================================")
         #self.target_net.set_weights(self.q_net.weights)
-        for i in range(len(self.target_net.weights)):
-            self.target_net.weights[i].assign(self.q_net.weights[i]) #代入
         
         x = np.ones(shape=(self.BATCH_SIZE, 4))
         ys1 = self.q_net(x).numpy().reshape(-1)
         ys2 = self.target_net(x).numpy().reshape(-1)
         for y1, y2 in zip(ys1, ys2):
             assert y1 == y2 #計算結果が同じでなければならない
-
 
 
 
@@ -222,19 +199,19 @@ def main():
             rewards: deque = deque(maxlen=100)
 
             while not done:
-                action = agent.get_action(state.reshape(1, 4))
+                action = agent.get_action(state)
                 next_state, reward, done, _ = env.step(action)
                 #print(f"reward:{reward}")
                 reward_sum = reward_sum + reward
                 #print(f"reward_sum:{reward_sum}")
                 #env.render()
-                agent.memory(state, action, reward, next_state, done)
+                loss = agent.memory(state, action, reward, next_state, done)
 
             rewards.append(reward_sum)
             avg_reward = statistics.mean(rewards)
         
             t.set_description(f"Episode {i}")
-            t.set_postfix(running_reward=reward_sum, avg_reward=avg_reward, epsilon=agent.eps)
+            t.set_postfix(running_reward=reward_sum, avg_reward=avg_reward, epsilon=agent.eps, steps=agent.t, loss=loss)
 
     pd.to_pickle("agent.pkl", agent)
 
--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Qnetwork(tf.keras.Model):
 
-    def __init__(self, num_actions, learning_rate=0.001):
+    def __init__(self, num_actions, learning_rate=0.0005):
 
         super().__init__()
 
@@ -34,9 +34,11 @@ class Qnetwork(tf.keras.Model):
         return self(state).numpy()
 
     def update(self, states, selected_actions, target_values):
-        selected_actions_onehot = tf.one_hot(selected_actions, self.num_actions)
 
         with tf.GradientTape() as tape:
+
+            selected_actions_onehot = tf.one_hot(selected_actions, self.num_actions)
+            
             #onehotを掛け選んだ行動のみを総和として算出(actionの次元をなくした)
             selected_actions_values = tf.reduce_sum(self(states) * selected_actions_onehot, axis=1) 
 
@@ -46,5 +48,21 @@ class Qnetwork(tf.keras.Model):
         variables = self.trainable_variables
         gradients = tape.gradient(loss, variables)
         self.optimizer.apply_gradients(zip(gradients, variables))
+
+        return loss
+
+
+    def loss(self, states, selected_actions, target_values):
+
+
+        selected_actions_onehot = tf.one_hot(selected_actions, self.num_actions)
+        
+        #onehotを掛け選んだ行動のみを総和として算出(actionの次元をなくした)
+        selected_actions_values = tf.reduce_sum(self(states) * selected_actions_onehot, axis=1) 
+
+        #分散の２つ目の求め方
+        loss = tf.reduce_mean(tf.square(target_values - selected_actions_values))
+
+        return loss
 
 
